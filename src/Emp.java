f9e4d6894c0194b331315894be91baeaf9a76fa0
santosh <santoshjoy41@gmail.com>
2024-05-21 14:48:27 +0530

    examples

---
+++ src/Emp.java
@@ -0,0 +1,17 @@
+public class Emp {
+
+ long age=21474836479l;
+ float amount =200.09f;
+ double cash=300.90;
+ char row='A';
+ boolean skyIsBlue=true;
+
+    public static void main(String[] args) {
+        Emp abc=new Emp();
+        System.out.println(abc.age);
+        System.out.println(abc.amount);
+        System.out.println(abc.cash);
+        System.out.println(abc.row);
+        System.out.println(abc.skyIsBlue);
+}
+}
